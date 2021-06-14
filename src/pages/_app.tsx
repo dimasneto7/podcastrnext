39bc1e57f2c570ff8db3c44236804ee71d3dec0a
Dimas Neto <dimasneto@pop-os.localdomain>
2021-06-14 20:49:55 -0300

    finish

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,22 +2,23 @@ import '../styles/global.scss';
 
 import { Header } from "../components/Header";
 import { Player } from "../components/Player";
-import { PlayerContext } from '../contexts/PlayerContext';
 
 import styles from '../styles/app.module.scss';
+import React, { useState } from 'react';
+import { PlayerContextProvider } from '../contexts/PlayerContext';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <PlayerContext.Provider value={'Dimas'}>
+  return(
+    <PlayerContextProvider>
       <div className={styles.wrapper}> 
         <main>
           <Header />
           <Component {...pageProps} />
         </main>
         <Player />
-    </div>
-    </PlayerContext.Provider>
-    
+      </div>
+    </PlayerContextProvider>
+      
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
-import { useContext } from 'react';
 import { GetStaticProps } from 'next';
 import Image from 'next/image';
+import Head from 'next/head';
 import Link from 'next/link';
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 
 import { api } from '../services/api';
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
-import { PlayerContext } from '../contexts/PlayerContext';
+import { usePlayer } from '../contexts/PlayerContext';
 
 import styles from './home.module.scss';
 
@@ -29,15 +29,20 @@ type HomeProps = {
 }
 
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
-  const player = useContext(PlayerContext)
+  const { playlist } = usePlayer()
+
+  const episodeList = [...latestEpisodes, ...allEpisodes];
 
   return (
     <div className={styles.homepage}>
+      <Head>
+        <title>Home | Podcastr</title>
+      </Head>
        <section className={styles.latestEpisodes}>
-        <h2>Últimos lançamentos {player}</h2>
+        <h2>Últimos lançamentos</h2>
 
         <ul>
-          {latestEpisodes.map(episode => {
+          {latestEpisodes.map((episode, index)=> {
             return (
               <li key={episode.id}>
                 <Image 
@@ -57,7 +62,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                   <span>{episode.durationAsString}</span>
                 </div>
 
-                <button type="button">
+                <button type="button" onClick={() => playlist(episodeList, index)}>
                   <img src="play-green.svg" alt="Tocar episódio" />
                 </button>
               </li>
@@ -81,7 +86,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
              </tr>            
            </thead>
            <tbody>
-             {allEpisodes.map(episode => {
+             {allEpisodes.map((episode, index) => {
                return (
                  <tr key={episode.id}>
                    <td style={{ width: 72 }}>
@@ -103,7 +108,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                    <td style={{ width: 100 }}>{episode.publishedAt}</td>
                    <td>{episode.durationAsString}</td>
                    <td> 
-                     <button type="button">
+                     <button type="button" onClick={() => playlist(episodeList, index + latestEpisodes.length)}>
                       <img src="/play-green.svg" alt="Tocar episódio" />
                      </button>
                    </td>
